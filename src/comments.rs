58502b19e5b8141282142004730451110a9e5e05
minwoo <mark42son@gmail.com>
2024-06-15 21:16:41 +0900

    easy rust ..to Integer part

---
+++ src/comments.rs
@@ -0,0 +1,19 @@
+// - documentation
+struct Book;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+/*
+
+블록을 주석으로 쓰고 싶다면..
+
+*/
+
+
+// 러스트를 조금씩 조금씩 배우기 시작
+
+// 그러나 아마 C를 먼저 더 열심히 진도를 나갈 것 같습니다. 
