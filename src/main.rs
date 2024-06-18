419812a203240524ee93577eef90acf3098f586f
minwoo <mark42son@gmail.com>
2024-06-18 21:06:11 +0900

    edit cargo enev and several files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("The smallest i8 is {} and the biggest i8 is {}.", i8::MIN, i8::MAX); // hint: printing std::i8::MIN means "print MIN inside of the i8 section in the standard library"
     println!("The smallest u8 is {} and the biggest u8 is {}.", u8::MIN, u8::MAX);
-    println!("The smallest i16 is {} and the biggest i16 is {}.", i16::MIN, i16::MAX);
+    println!("The smallest i16 i {} and the biggest i16 is {}.", i16::MIN, i16::MAX);
     println!("The smallest u16 is {} and the biggest u16 is {}.", u16::MIN, u16::MAX);
     println!("The smallest i32 is {} and the biggest i32 is {}.", i32::MIN, i32::MAX);
     println!("The smallest u32 is {} and the biggest u32 is {}.", u32::MIN, u32::MAX);
