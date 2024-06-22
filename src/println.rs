26e8e20e0d0a9f82c52de3a30abc0ff93fcaeb49
minwoo <mark42son@gmail.com>
2024-06-22 21:58:37 +0900

    create println-1 file

---
+++ src/println.rs
@@ -0,0 +1,18 @@
+// macro = function that writes code
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+//   '\n' -> 'new line'을 의미하는걸 이제 알았다.. ㄷ;
+
+
+// // macro = function that writes code
+
+// fn main() {
+//     let my_name = "David";
+//     let my_age = 42;
+//     println!("My name is {}, and my age is {}", my_name, my_age);
+// }
